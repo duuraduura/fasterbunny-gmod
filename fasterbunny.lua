75348114c2cc967065ecb088333ac23765f05700
nsocOnigiriChan <needysz-ai@rambler.ru>
2020-08-30 21:00:41 +0300

    Добавлено: показатель скорости игрока, AutoDucking (авто-присяд), NoClip чекер

--- fasterbunny.lua
+++ fasterbunny.lua
@@ -1,41 +1,98 @@
+-- Авторство
 chat.AddText(
 	Color(255, 255, 255), 'Скрипт разработал (Telegram ID): ', 
-	Color(255, 100, 100), '@amikoqfree'
+	Color(255, 100, 100), '@SepultureSE'
 	)
---
 
--- Переменные
+-- Клиентские ConVar переменные
 CreateClientConVar('isBunny', 1, true, false)
-CreateClientConVar('playerVelocity', LocalPlayer():GetVelocity(), true, false)
---
+CreateClientConVar('isAutoDuck', 1, true, false)
 
+CreateClientConVar('option_GUI_Velocity', 1, true, false)
+
+-- Команда-switch`ер для включения/выключения fasterbunny
 concommand.Add('.bunny', function( )
 	if GetConVar('isBunny'):GetInt() == 0 then
+		
 		GetConVar('isBunny'):SetInt(1)
+		GetConVar('option_GUI_Velocity'):SetInt(1)
+		GetConVar('isAutoDuck'):SetInt(1)
+
 		RunConsoleCommand('+speed')
 		chat.AddText(Color(100, 255, 100), '[+] FasterBunny - ON')
-		LocalPlayer():SetFOV( LocalPlayer():GetFOV() + 50 )
 	
 	else
 		GetConVar('isBunny'):SetInt(0)
+		GetConVar('option_GUI_Velocity'):SetInt(0)
+		GetConVar('isAutoDuck'):SetInt(0)
+
 		RunConsoleCommand('-speed')
 		chat.AddText(Color(255, 100, 100), '[-] FasterBunny - OFF')
 		timer.Destroy('Bhop')
 	end
 end )
 
-function Bunnyhop()
-	if GetConVar('isBunny'):GetInt() == 1 then
-	 	if input.IsKeyDown(KEY_SPACE) then
-	 		if LocalPlayer():IsOnGround() then
-	 			RunConsoleCommand('+jump')
+-- Функция для получения данных об игроке
+function GetPlayerData( )
+	PlayerVelocity = LocalPlayer():GetVelocity()
+	PlayerSpeed = PlayerVelocity:Length()
+	Ypos = PlayerVelocity[3]
+end
 
-	 			timer.Create('Bhop', 0, 0, function()
-	 				RunConsoleCommand('-jump')
-	 			end )
-	 		end
+-- Функция для bunnyhop`а
+function BunnyScript()
+	if GetConVar('isBunny'):GetInt() == 1 then
+	 	if input.IsKeyDown(KEY_SPACE) and LocalPlayer():IsOnGround() then
+ 			RunConsoleCommand('+jump')
+ 			timer.Create('Bhop', 0, 0, function()
+ 				RunConsoleCommand('-jump')
+ 			end )
  		end
  	end
 end
 
-hook.Add( 'Think', 'FasterBunnyScript', Bunnyhop )
+-- Функция autoDucking
+function AutoDuck( )
+	if GetConVar('isAutoDuck'):GetInt() == 1 then
+		if LocalPlayer():IsOnGround() == false then
+			if math.Round(Ypos) >= 50 then
+				RunConsoleCommand('+duck')
+			elseif math.Round(Ypos) <= -10 then
+				RunConsoleCommand('-duck')
+			end
+		else
+			RunConsoleCommand('-duck')
+		end
+	end
+end
+
+-- Функция для GUI скорости
+function GUI_Velocity( )
+	if GetConVar('option_GUI_Velocity'):GetInt() == 1 then
+		draw.DrawText('Velocity: ' .. math.Round(PlayerSpeed),
+			'DermaLarge', 
+			50, 
+			50, 
+			Color(255,255,255,255),
+			TEXT_ALIGN_LEFT )
+	end
+end
+
+-- Проверка NoClip`а
+function NoClipChecker( )
+	if GetConVar('isBunny'):GetInt() == 1 then
+		RunConsoleCommand('.bunny')
+		RunConsoleCommand('-duck')
+	end
+end
+
+-- Hook`и
+-- Функционал скрипта:
+hook.Add( 'Think', 'GetPlayerData', GetPlayerData ) -- Получение данных об игроке
+
+hook.Add( 'Think', 'AutoDuck', AutoDuck ) -- AutoDucking
+hook.Add( 'Think', 'FasterBunnyScript', BunnyScript ) -- Bunnyhop
+hook.Add( 'PlayerNoClip', 'NoClipChecker', NoClipChecker ) -- Noclip чекер
+
+-- GUI:
+hook.Add( 'HUDPaint', 'FasterBunny_Velocity', GUI_Velocity )
